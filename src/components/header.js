8c7a518941ce61c942b98a6965f00c40adfb7281
bemijonathan <bemijonathan@gmail.com>
2020-09-17 03:29:51 +0100

    started mobile optimization

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,12 @@
 import React from "react";
-// import image1 from '../images/banner1.png'
 import Button from "./button";
-// buttonVariat
 
 function Header() {
   return (
-    <section className="flex items-center justify-center">
-      <div className="w-3/5 __banner_bg1 min-h-screen relative">
+    <section className="lg:flex items-center justify-center ">
+      <div className="lg:w-3/5 __banner_bg1 min-h-screen relative">
         <div className="absolute top-0 bottom-0  left-0 right-0 h-full __blue-background">
-          <div className="w-full sm:px-8 lg:px-16 xl:px-20 2xl:px-64 mt-half-screen">
+          <div className="w-full px-3 lg:px-16 xl:px-20 2xl:px-64 mt-half-screen md:w-2/3 lg:w-full ">
             <h1 className="text-h1 font-bold mb-8 text-white">The Next Billion Creators </h1>
             <h4 className="text-white text-h5 mb-8 lg:w-2/3 ">
               A community of creative minds driving the Open Source movement in Africa.{" "}
@@ -22,11 +20,11 @@ function Header() {
         </div>
       </div>
       <aside className="flex-1">
-        <div className="h-half-screen relative __banner_bg2">
+        <div className="h-half-screen md:py-10 relative __banner_bg2">
           <div className="absolute top-0 bottom-0  left-0 right-0 h-full __community-background">
-            <div className="w-full sm:px-8 lg:px-16 xl:px-20 2xl:px-64">
+            <div className="w-full px-3 lg:px-16 xl:px-20 2xl:px-64 md:w-2/3 lg:w-full">
               <h3 className="text-2xl font-black text-white"> Community </h3>
-              <h5 className="text-white text-base mt-4">
+              <h5 className="text-white text-base mt-4 ">
                 Our community is spread across Africa hosted in various locations called chapters.
               </h5>
               <p className="text-white mt-12 text-sm cursor-pointer __check_out">
@@ -47,7 +45,7 @@ function Header() {
         </div>
         <div className="h-half-screen relative __banner_bg3">
           <div className="absolute top-0 bottom-0  left-0 right-0 h-full __team-background">
-            <div className="w-full sm:px-8 lg:px-16 xl:px-20 2xl:px-64">
+            <div className="w-full px-3 lg:px-16 xl:px-20 2xl:px-64">
               <h3 className="text-2xl font-black text-white"> Team </h3>
               <h5 className="text-white mt-4">The brains behind the awesomeness.</h5>
               <p className="text-white mt-12 text-sm cursor-pointer __check_out">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,8 @@
 import React from "react";
-
 import Layout from "../components/layout";
 import SEO from "../components/SEO/seo";
-
 import Nav from "../components/nav";
 import Header from "../components/header";
-// import Footer from "../components/footer";
 
 function IndexPage() {
   return (
@@ -13,7 +10,6 @@ function IndexPage() {
       <SEO keywords={["Open Source Community Africa", "OSCA", "Open Source Festival", "OSF"]} title="Home" />
       <Nav />
       <Header />
-      {/**<Footer /> **/}
     </Layout>
   );
 }
