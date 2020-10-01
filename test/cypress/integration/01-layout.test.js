46b0dd58e1092ebc8bf2a82e513f0c066c9d2f4f
Oluebube Princess Egbuna <egbunaoluebube@gmail.com>
2020-10-01 16:40:34 +0100

    Update tests

--- test/cypress/integration/01-layout.test.js
+++ test/cypress/integration/01-layout.test.js
@@ -4,10 +4,6 @@ describe("home page", () => {
     cy.visit("/");
   });
 
-  it('should have text "A community of creative minds driving the Open Source movement in Africa"', () => {
-    cy.contains("A community of creative minds driving the Open Source movement in Africa");
-  });
-
   it("header should have 8 anchor tags", () => {
     cy.get("header a").should("have.length", 6);
   });
