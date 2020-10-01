4a625d5b8d98edf309182b66b76b7a1a0c9d2085
Oluebube Princess Egbuna <egbunaoluebube@gmail.com>
2020-10-01 16:17:07 +0100

    Update tests

--- test/cypress/integration/01-layout.test.js
+++ test/cypress/integration/01-layout.test.js
@@ -11,8 +11,4 @@ describe("home page", () => {
   it("header should have 8 anchor tags", () => {
     cy.get("header a").should("have.length", 6);
   });
-
-  it("footer should have 10 anchor tags", () => {
-    cy.get("footer a").should("have.length", 10);
-  });
 });
